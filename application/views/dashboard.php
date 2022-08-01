a44c973abf461947878d140c92e929ff8a0f973e
anfasaadefatra <anfasaadefatra21@gmail.com>
2022-08-01 20:10:46 +0800

    First commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,33 @@
+<!-- Default box -->
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Halaman Utama</h3>
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                <i class="fas fa-minus"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                <i class="fas fa-times"></i></button>
+        </div>
+    </div>
+    <div class="card-body">
+        <img src="<?= base_url('assets/logo.jpg') ?>" class="img-fluid" alt="Responsive image" width="25%">
+        <div class="form-group" style="padding-top: 20px;">
+            <p>Selamat Datang <b>Anfa</b> di Aplikasi Sistem Pendukung Keputusan Penentuan Jurusan <b>SMK Negeri 3 Palu</b></p>
+            <h3>VISI</h3>
+            <ul>
+                <li>Mewujudkan Mutu Lulusan yang berImtaq serta Berdaya Saing tinggi di Era Global.</li>
+                <li>Menghasilkan Tamatan yang Kompeten, Kompetitif dan Profeional</li>
+            </ul>
+            <h3>MISI</h3>
+            <ul>
+                <li>Menyiapkan Siswa yang Profesional untuk memasuki Dunia Kerja serta Mengembangkan Sikap Prfesional</li>
+                <li>Menyiapkan Siswa agar mampu Memiliki Karir, mampu Berkompetisi dan mampu Mengembangkan diri</li>
+                <li>Menyiapkan Tenaga Kerja Tingkat Menengah untuk Mengisi Kebutuhan Dunia Usaha dan Industri saat ini maupun pada saat mendatang</li>
+            </ul>
+        </div>
+    </div>
+    <!-- /.card-body -->
+    <!-- /.card-footer-->
+</div>
+<!-- /.card -->
